9827d71990c15aa3ab23f16a331bcb6870a1417c
Andrew Seward <awswrd@gmail.com>
2020-12-12 20:56:56 -0800

    Update SJ access names

--- sj_pull.sh
+++ sj_pull.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 readonly url="$1"
 readonly sqlite_filepath="$(mktemp -t pulled_sqlite-XXXXXXXX.db)"
-readonly sj_access="${SJ_ACCESS:-test-read}"
+readonly sj_access="${SJ_ACCESS:-dw-read}"
 
 echo "${sj_access}" | xargs -t >&2 uplink cp "${url}" "${sqlite_filepath}" --progress=false --access
 
