48e5f0fa73c7668603fd8176b94cf18ef15a630a
Andrew Seward <awswrd@gmail.com>
2021-12-16 22:55:48 -0800

    … 🤔

--- http.sh
+++ http.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # socat -v -v -d -d TCP-LISTEN:8080,reuseaddr,fork exec:'./http.sh exec ./handler.sh',pipes,crnl
 exec() {
-  echo -e "HTTP/1.1 202 Accepted\nConnection: close\n\nUhhhh"
+  echo -e "HTTP/1.1 202 Accepted\n\nUhhhh"
   return 0
   # ---
 
