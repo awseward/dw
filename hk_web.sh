0b2342b40a1ef5a5eee23119d375dba6abfea682
Andrew Seward <awswrd@gmail.com>
2021-12-16 23:01:01 -0800

    ???

--- hk_web.sh
+++ hk_web.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 export PATH="/app/bin:${PATH}"
 
-socat -v -v -d -d "TCP-LISTEN:${PORT}",reuseaddr,fork exec:'./http.sh exec ./handler.sh',pipes,crnl
+socat -v -v -d -d "TCP-LISTEN:${PORT}",reuseaddr,exec:'./http.sh exec ./handler.sh',pipes,crnl
