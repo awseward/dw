c67056f918888ad4090dd3082e3c0421f583226b
Andrew Seward <awswrd@gmail.com>
2021-12-16 23:55:26 -0800

    …

--- http.sh
+++ http.sh
@@ -7,6 +7,7 @@ exec() {
   # === BEGIN https://stackoverflow.com/a/63057984 workaround ===
   local full_request
   while read -r line; do
+    >&2 echo ">>>>>>>>>> ${line}"
     grep -e '^[A-Za-z]' <<< "${line}" >/dev/null || break;
 
     if [ "${full_request:-}" = '' ]; then
