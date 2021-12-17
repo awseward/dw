1613e29bd8f9434e49b09ff5866e502f810c1d68
Andrew Seward <awswrd@gmail.com>
2021-12-16 22:12:41 -0800

    Pull out actually loading bits
    Should make it much simpler to plug the new socat-based `http/handler`
    stuff into.

---
+++ load.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# === BEGIN 🤷 ===
+access_name="$(mktemp -u -t sj-access-XXXXXXXXXXXXXXXX | xargs basename)"
+readonly access_name="${access_name,,}"
+uplink import "${access_name}" "${SJ_ACCESS}"
+export SJ_ACCESS="${access_name}"
+# === END 🤷 ===
+
+run() {
+  local -r store="$1"
+  local -r sj_path="$2"
+
+  echo "${sj_path}" \
+    | xargs -t ./sj_pull.sh \
+    | xargs -t -I{} ./pg_load.sh {} "${store}"
+}
+
+run "$@"
--- server.sh
+++ server.sh
@@ -4,12 +4,6 @@
 
 readonly response="HTTP/1.1 202 Accepted\r\nConnection: close\r\n\r\n${2:-"Accepted"}\r\n"
 
-access_name="$(mktemp -u -t sj-access-XXXXXXXXXXXXXXXX | xargs basename)"
-readonly access_name="${access_name,,}"
-
-uplink import "${access_name}" "${SJ_ACCESS}"
-export SJ_ACCESS="${access_name}"
-
 while true; do
   ( set -euo pipefail
 
@@ -26,14 +20,12 @@ while true; do
         # shellcheck disable=SC2001
         store="$(echo "${request_url}" | sed -e 's/^.*store=\([^& ]*\).*$/\1/')"
         readonly store="${store,,}"
-
         # shellcheck disable=SC2001
-        readonly sj_path="$(echo "${request_url}" | sed -e 's/^.*sj_path=\([^& ]*\).*$/\1/')"
+        readonly sj_path="$(
+          echo "${request_url}" | sed -e 's/^.*sj_path=\([^& ]*\).*$/\1/'
+        )"
         echo "${path} -- sj_path=${sj_path} store=${store}"
-
-        echo "${sj_path}" \
-          | xargs -t ./sj_pull.sh \
-          | xargs -t -I{} ./pg_load.sh {} "${store}"
+        ./load.sh "${store}" "${sj_path}"
         ;;
 
       (*) >&2 echo "[ERR] Rejected: ${path}"
